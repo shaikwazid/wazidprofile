9f3f435760ea96d28ba55972dea07e19c85a5235
shaikwazid <shaikwazid2705@gmail.com>
2025-10-23 14:27:58 +0530

    index and app jsx are changed for default showing component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';  
-import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import MainNav from './Components/MainNav/MainNav';
 import Profile from './Components/Profile/Profile';
 import About from './Components/About/About';
@@ -14,20 +14,25 @@ import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
+      {/* Navbar */}
       <MainNav />
 
-       <div className="flex-grow-1 mt-5">
-         <Routes>
-          <Route path ="/" element = {<Profile />}> </Route>
-          <Route path ="/about" element = {<About />}></Route>
-          <Route path ="/skills" element = {<Skills />}></Route>
-          <Route path ="/projects" element = {<Projects />}></Route>
-          <Route path ="/contact" element = {<Contact />}></Route>
+      {/* Page Content */}
+      <div className="flex-grow-1 mt-5">
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          
+          {/* Redirect all unknown paths to home */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-       </div>
+      </div>
 
-       <Footer />
-      
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
